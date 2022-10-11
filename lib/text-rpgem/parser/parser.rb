7807f09f5cd83130bbb4d698d46e5f193e1a6474
Maksim Nezhinskii <maksimnezhin@gmail.com>
2022-10-11 20:06:56 +0300

    Created class for parsing errors and included it in validation methods

--- lib/text-rpgem/parser/parser.rb
+++ lib/text-rpgem/parser/parser.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "parser_errors"
+
 # Module for parsing events from text files
 module Parser
   module_function
@@ -11,24 +13,24 @@ module Parser
     text = File.read(path)
 
     description = text[/(?<={)(.|\s)*?(?=[^\\]})/]
-    Validation.validate_emptiness description, "Description"
-    Validation.validate_extra_brackets description, "Description"
+    Validation.validate_emptiness description, "Description", path
+    Validation.validate_extra_brackets description, "Description", path
     description&.strip!
 
     options_block = text[/[^\\]\[(.|\s)*[^\\]\]/]
-    Validation.validate_emptiness options_block, "OptionsBlock"
-    options = get_options_hash options_block
+    Validation.validate_emptiness options_block, "OptionsBlock", path
+    options = get_options_hash options_block, path
     [description, options]
   end
 
   # A method for transform options block
   # @param options_block [String,nil] lines with options description from file
   # @return [Hash] hash of the form {'alias for option': 'option description'}
-  def get_options_hash(options_block)
+  def get_options_hash(options_block, file_name)
     options = {}
 
     options_block&.scan(/[^\\]\[((.|\s)*?)[^\\]\]/) do |option|
-      Validation.validate_option option[0]
+      Validation.validate_option option[0], file_name
       option_alias, option_desc = option[0]&.split "::"
       options[option_alias] = option_desc
     end
@@ -39,25 +41,22 @@ module Parser
   module Validation
     module_function
 
-    def validate_emptiness(block, block_name)
-      if block.nil? || block.empty? || !block.match(/\A\s*\z/).nil?
-        raise "Empty#{block_name}" # TODO: Exception
-      end
+    def validate_emptiness(block, block_name, file_name)
+      raise ParsingError.new "Empty #{block_name}", file_name if block.nil? || !block.match(/\A\s*\z/).nil?
     end
 
-    def validate_extra_brackets(block, block_name)
-      raise "#{block_name}IncludeExtraBrackets" unless block.match(/[^\\][{}\[\]]/).nil? # TODO: Exception
+    def validate_extra_brackets(block, block_name, file_name)
+      raise ParsingError.new "#{block_name} includes extra brackets", file_name unless block.match(/[^\\][{}\[\]]/).nil?
     end
 
-    def validate_option(option)
-      raise "WrongOptionFormat" unless option.include? "::" # TODO: Exception
+    def validate_option(option, file_name)
+      raise ParsingError.new "Wrong option format", file_name unless option.include? "::" # TODO: Exception
 
       option_alias, option_desc = option.split "::"
-      validate_emptiness option_alias, :OptionAlias
-      validate_extra_brackets option_alias, :OptionAlias
-      validate_emptiness option_desc, :OptionDescription
-      validate_extra_brackets option_desc, :OptionDescription
+      validate_emptiness option_alias, :OptionAlias, file_name
+      validate_extra_brackets option_alias, :OptionAlias, file_name
+      validate_emptiness option_desc, :OptionDescription, file_name
+      validate_extra_brackets option_desc, :OptionDescription, file_name
     end
   end
 end
-
