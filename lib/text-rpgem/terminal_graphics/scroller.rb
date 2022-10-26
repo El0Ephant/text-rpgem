19cac7f9a0fcf63bfacd8f0b636ea8aacc1066b1
Aleksandr Kalinin <akalinin@sfedu.ru>
2022-10-26 17:21:18 +0300

    Added scroller

---
+++ lib/text-rpgem/terminal_graphics/scroller.rb
@@ -0,0 +1,70 @@
+class Scroller
+  attr_accessor :count,:line,:pos,:sz,:last, :body # fields
+  def initialize (count) # constructor
+    @count = count # count of strings
+    @sz = 23 # size
+    k = 1 # movement
+    t = 1 # fixer
+    step = 0.2
+    @line = [1]*sz # scroller
+    @body = [0]*count
+    while count > sz*t
+      t += step
+      @line[sz-k] = 0
+      k+=1
+      if @sz*t > 100
+        step +=1
+      end
+      if @sz* t > 300
+        break
+      end
+      if  count > 1000
+        @line[1] = 0
+      end
+    end
+    @last = 0
+    for i in 0..@line.length-1
+      @body[i] = @line[i]
+      if @body[i] == 1
+        @last = i
+      end
+      end
+    @pos = 0
+  end
+  def move_down()
+    if @count > @sz
+      @pos += 1
+      @body[pos-1] = 0
+      @body[pos+last] = 1
+
+    end
+  end
+  def move_up()
+    if @count > @sz
+      @pos -= 1
+      @body[pos] = 1
+      @body[pos+last+1] = 0
+    end
+  end
+
+  end
+ s = Scroller.new(80)
+
+count = s.count
+str = "text                             "
+
+s.move_down()
+s.move_down()
+s.move_up()
+
+arr = s.body
+for i in 0..count-1
+  if arr[i] == 1
+    puts(str + "█\n")
+  end
+  if arr[i] == 0
+    puts(str + " \n")
+  end
+
+end
+
