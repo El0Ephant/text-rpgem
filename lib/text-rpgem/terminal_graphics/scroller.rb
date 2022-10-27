28510bb8c8cabf1083f0d820dbae6b349233dc14
Ksum7 <xamchik02@gmail.com>
2022-10-28 00:55:11 +0300

    reworked scroller

--- lib/text-rpgem/terminal_graphics/scroller.rb
+++ lib/text-rpgem/terminal_graphics/scroller.rb
@@ -1,70 +1,28 @@
+# frozen_string_literal: false
+
+# Scroller class
 class Scroller
-  attr_accessor :count,:line,:pos,:sz,:last, :body # fields
+  attr_reader :result_arr
 
-  def initialize (count) # constructor
-    @count = count # count of strings
-    @sz = 23 # size
-    k = 1 # movement
-    t = 1 # fixer
-    step = 0.2
-    @line = [1] * sz # scroller
-    @body = [0] * count
-    while count > sz * t
-      t += step
-      @line[sz-k] = 0
-      k+=1
-      if @sz * t > 100
-        step +=1
-      end
-      if @sz * t > 300
-        break
-      end
-      if  count > 1000
-        @line[1] = 0
-      end
-    end
-    @last = 0
-    (0..@line.length - 1).each { |i|
-      @body[i] = @line[i]
-      if @body[i] == 1
-        @last = i
-      end
-    }
-    @pos = 0
-  end
-  def move_down
-    if @count > @sz
-      @pos += 1
-      @body[pos-1] = 0
-      @body[pos+last] = 1
-
-    end
-  end
-  def move_up
-    if @count > @sz
-      @pos -= 1
-      @body[pos] = 1
-      @body[pos + last + 1] = 0
+  def initialize(length)
+    @length = length
+    @position = 0
+    @result_arr = Array.new(23) { [" "] }
+    @scroller_length = [(23.0 / (length + 1) * 23).ceil, 23].min
+    @scroller_length&.times do |i|
+      @result_arr[i][0] = "█"
     end
   end
 
+  def move(direction)
+    if (@position * 23 / @length) != ((@position + direction) * 23 / @length)
+      if direction.negative?
+        @result_arr.rotate! 1
+      else
+        @result_arr.rotate!(-1)
+      end
+    end
+    @position += direction
   end
- s = Scroller.new(80)
 
-count = s.count
-str = "text                             "
-
-s.move_down()
-s.move_down()
-s.move_up()
-
-arr = s.body
-(0..count - 1).each { |i|
-  if arr[i] == 1
-    puts(str + "█\n")
-  end
-  if arr[i] == 0
-    puts(str + " \n")
-  end
-}
-
+end
