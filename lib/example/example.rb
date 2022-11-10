e86ad89edb9b1bb50045bf2c2630c85213004585
El0Ephant <75036763+El0Ephant@users.noreply.github.com>
2022-11-11 00:59:01 +0300

    example (work in progress)

--- lib/example/example.rb
+++ lib/example/example.rb
@@ -1,13 +1,16 @@
 require_relative '../text-rpgem/text_rpgem'
 
-path = "#{File.dirname(__FILE__ )}/source/"
+path = "#{File.dirname(__FILE__)}/source/"
 
 example_scenario = Scenario.new(
   counters: {
     strength: Counter.new(7),
     coins: Counter.new(500),
-    mana_potions: Counter.new(0),
+    health_potions: Counter.new(0),
     hp: Bar.new(10, 10),
+  },
+  hidden_counters: {
+    swords: Counter.new(1),
   },
   events: {
     beginning: Event.new("#{path}beginning.txt"),
@@ -15,8 +18,16 @@ example_scenario = Scenario.new(
     out: Event.new("#{path}out.txt"),
     shop: Event.new("#{path}shop.txt"),
     shop_without_sword: Event.new("#{path}shop_without_sword.txt"),
+    crossroad: Event.new("#{path}crossroad.txt"),
+    question: Event.new("#{path}question.txt"),
+    retreat: Event.new("#{path}crossroad.txt"),
+    dragon: Event.new("#{path}dragon.txt"),
+    fight: Event.new("#{path}fight.txt"),
+    death: Event.new("#{path}death.txt"),
+    victory: Event.new("#{path}victory.txt"),
   },
-) do |events, counters|
+) do |events, counters, hidden_counters|
+  # Shop section
   events[:beginning].routes(
     shop: events[:shop].routes_by_lambda(
       lambda do |option|
@@ -25,19 +36,21 @@ example_scenario = Scenario.new(
           return events[:not_enough_money] if counters[:coins].value < 300
 
           counters[:coins].value -= 300
+          hidden_counters[:swords].value -= 1
           counters[:strength].value += 3
-          return events[:shop_without_sword]
+          return hidden_counters[:swords].value.zero? ? events[:shop_without_sword] : events[:shop]
         when :potion_bought
           return events[:not_enough_money] if counters[:coins].value < 100
 
           counters[:coins].value -= 100
-          counters[:mana_potions].value += 1
+          counters[:health_potions].value += 1
           return events[:shop]
         when :leave
           return events[:out]
         end
       end
     ),
+    start: events[:out],
   )
 
   events[:shop_without_sword].routes_by_lambda(
@@ -47,7 +60,7 @@ example_scenario = Scenario.new(
         return events[:not_enough_money] if counters[:coins].value < 100
 
         counters[:coins].value -= 100
-        counters[:mana_potions].value += 1
+        counters[:health_potions].value += 1
         return events[:shop_without_sword]
       when :leave
         return events[:out]
@@ -55,6 +68,21 @@ example_scenario = Scenario.new(
     end
   )
 
+  events[:not_enough_money].routes_by_lambda(
+    lambda do |option|
+      case option
+      when :continue
+        return hidden_counters[:swords].value.zero? ? events[:shop_without_sword] : events[:shop]
+      end
+    end
+  )
+
+  # fight section
+  events[:out].routes(
+    {
+      start: Events()
+    }
+  )
 end
 
 Window.new(example_scenario).run
